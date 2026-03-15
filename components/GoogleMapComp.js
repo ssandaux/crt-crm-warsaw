c2d445ffcc1cc87dab5a63c53f3ff45ce4e42c7b
ssandaux <amelidreamzmgmt@gmail.com>
2026-03-15 17:17:54 +0100

    Show business POI markers on map (hide only parks, schools, etc.)

--- components/GoogleMapComp.js
+++ components/GoogleMapComp.js
@@ -22,7 +22,12 @@ const MAP_OPTIONS = {
     strictBounds: false,
   },
   styles: [
-    { featureType: 'poi', stylers: [{ visibility: 'off' }] },
+    { featureType: 'poi.park', stylers: [{ visibility: 'off' }] },
+    { featureType: 'poi.place_of_worship', stylers: [{ visibility: 'off' }] },
+    { featureType: 'poi.school', stylers: [{ visibility: 'off' }] },
+    { featureType: 'poi.sports_complex', stylers: [{ visibility: 'off' }] },
+    { featureType: 'poi.medical', stylers: [{ visibility: 'off' }] },
+    { featureType: 'poi.government', stylers: [{ visibility: 'off' }] },
     { featureType: 'transit', stylers: [{ visibility: 'off' }] },
     { featureType: 'road', elementType: 'labels.icon', stylers: [{ visibility: 'off' }] },
     { featureType: 'road.highway', elementType: 'labels.text', stylers: [{ visibility: 'off' }] },
